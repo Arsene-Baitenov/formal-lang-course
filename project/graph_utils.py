63eb63b34d29c757d2650e560e3ed5608cf97b01
Arsene-Baitenov <arsenebaitenov@gmail.com>
2024-09-16 23:48:08 +0300

    feat: Add function to convert regex and graph to finite automaton.

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -1,6 +1,8 @@
-from typing import Tuple
+from typing import Tuple, Set
 import cfpq_data
 import networkx as nx
+from networkx import MultiDiGraph
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, State
 
 from project.graph import Graph
 
@@ -21,3 +23,26 @@ def create_labeled_two_cycles_graph(
         n=num_first_cycle_nodes, m=num_second_cycle_nodes, labels=labels
     )
     nx.nx_pydot.write_dot(graph, path)
+
+
+def graph_to_nfa(
+    graph: MultiDiGraph, start_states: Set[int], final_states: Set[int]
+) -> NondeterministicFiniteAutomaton:
+    nfa: NondeterministicFiniteAutomaton = (
+        NondeterministicFiniteAutomaton.from_networkx(
+            graph
+        ).remove_epsilon_transitions()
+    )
+
+    nodes = [int(node) for node in graph.nodes]
+
+    start_states = start_states if start_states else nodes
+    final_states = final_states if final_states else nodes
+
+    for state in start_states:
+        nfa.add_start_state(State(state))
+
+    for state in final_states:
+        nfa.add_final_state(State(state))
+
+    return nfa
